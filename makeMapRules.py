549ffc5003c7df87324993fe3084ac191598eeae
Adam Spryszynski <as2569@njit.edu>
2018-06-06 14:26:44 -0400

    dynamic cell list

--- makeMapRules.py
+++ makeMapRules.py
@@ -8,63 +8,75 @@ class CellRow:
     def __init__(self, rowNum, frame):
         self.frame = frame
         self.rowNum = rowNum
-        self.cellId = tk.Label(self.frame, text = str(rowNum), font = ('Helvetica', 20))
-        self.cellName = tk.Label(self.frame, text = 'Initial', font = ('Helvetica', 20))
+        self.cellId = tk.Label(self.frame, text = str(rowNum), font = ('Helvetica', 20)) # do I even need to provide params?
+        self.cellName = tk.Label(self.frame, text = 'Empty', font = ('Helvetica', 20))
         self.cellColor = tk.Canvas(self.frame, width=20, height=20, bg="black")
-        self.deleteButton = tk.Button(self.frame, text = "delete", command = self.ClearRow)
+        self.deleteButton = tk.Button(self.frame, text = "Delete", command = self.ClearRow)
 
     def UpdateRow(self, rowNum, rowData):
-        #cellId = tk.Label(self.frame, text = str(orderNumber), font = ('Helvetica', 20))
+        newRowData = rowData
+        if newRowData is None:
+            newRowData = ['Empty',(0,0,0,0)]
+            
         self.cellId['text'] = str(rowNum)
-        self.cellId.grid(column = 0, row = rowNum)
+        self.cellId.grid(column = 0, row = rowNum+1)
 
-        #cellName = tk.Label(self.frame, text = rowData[0], font = ('Helvetica', 20))
-        self.cellName['text'] = rowData[0]
-        self.cellName.grid(column = 1, row = rowNum)
+        self.cellName['text'] = newRowData[0]
+        self.cellName.grid(column = 1, row = rowNum+1)
 
-        #self.cellColor = tk.Canvas(self.frame, width=20, height=20, bg="black")
-        self.cellColor.grid(column = 2, row = rowNum)
+        self.cellColor.grid(column = 2, row = rowNum+1)
 
-        self.deleteButton.grid(column = 3, row = rowNum)
+        self.deleteButton.grid(column = 3, row = rowNum+1)
 
     def ClearRow(self):
-        data.cellList.row_data[self.rowNum] = ['Empty', (0,0,0,0)]
+        data.cellList.row_data[self.rowNum] = [None]
+        data.cellList.SortRowData()
         data.cellList.UpdateRows()
-        print('clear row' + str(self.rowNum))
-        
+        print('clear row ' + str(self.rowNum))
         
 class CellList:
     def __init__(self):
         self.maxCells = 10
-        self.row_data = [['Initial', (0,0,0,0)]] * self.maxCells
+        self.row_data = [None] * self.maxCells
         self.row_gui = []
         self.cellListFrame = tk.Frame(master)
         self.cellListFrame.grid(column=0, row=0)
-        self.CreateRows()
+        self.CreateCellList()
         self.UpdateRows()
 
-    def CreateRows(self):
-        self.cellListLabel = tk.Label(self.cellListFrame, text="Cell List", font=('Helvetica', 20))
+    def CreateCellList(self):
+        #Cell list label
+        self.cellListLabel = tk.Label(self.cellListFrame, text="Cell List", font=('Helvetica', 20)) 
         self.cellListLabel.grid(column=0, row=0, columnspan=4)
+        #Cell rows
         for i in range(0, self.maxCells):
             cellRow = CellRow(i, self.cellListFrame)
             self.row_gui.append(cellRow)
+        #Add cell label    
+        self.addCellLabel = tk.Label(self.cellListFrame, text = "Add cell", font = ('Helvetica', 20))
+        self.addCellLabel.grid(column=0, row=self.maxCells+1, columnspan=4)
+        #Add cell widgets
+        self.addCellName = tk.Entry(self.cellListFrame)
+        self.addCellName.grid(column=0, row=self.maxCells+2)
+        self.addCellButton = tk.Button(self.cellListFrame, text = "Add cell", command = self.AddCell)
+        self.addCellButton.grid(column=1, row=self.maxCells+2)
 
+    def SortRowData(self):
+        print(str(len(self.row_data)))
+        for i in range(0, len(self.row_data)):
+            if self.row_data[i] is None:
+                self.row_data.pop(i)
+                self.row_data.append([None])
+        print(str(len(self.row_data)))
+        
     def UpdateRows(self):
+        self.SortRowData()
         for i in range(0, self.maxCells):
             self.row_gui[i].UpdateRow(i, self.row_data[i])
 
-    def Delete_cell_data(self, inInt):
-        print('test ' + str(inInt))
-
-    def create_cell_add_gui(self):
-        addCellLabel = tk.Label(self.cellListFrame, text = "add cell", font = ('Times', 20))
-        addCellLabel.grid(column = 0, row = 15)
-
-def add_to_list(self):
-    print("adding to list")
-
-
+    def AddCell(self):
+        print('adding cell')
+        
 root = tk.Tk()
 root.geometry('1200x800')
 
@@ -72,11 +84,6 @@ master = tk.Frame(root)
 master.grid(column=0, row=0)
 
 data = Data()
-
-#this_cellList = CellList()
-
-#this_cellList.create_cell_list()
-#this_cellList.create_cell_add_gui()
 
 root.mainloop()
 
